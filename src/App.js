7fc919068b961dbe80a7c0f2e33f8685fa9cccb7
mirindafernando <mirindafernando2018@gmail.com>
2022-11-26 00:03:39 -0300

    Subindo exercicio

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import BotaoAcao from "./componentes/BotaoAcao";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BotaoAcao />
+      </div>
+    );
+  }
+}
+export default App;
